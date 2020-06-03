7b0fdcd0a09985759aac3a52110e06c5b3b37a1b
Tim Wanders <tim241@mailbox.org>
2020-06-03 13:21:58 +0200

    Android: fix wrong device codename

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter channel, $(TARGET_DEVICE)),)
+ifneq ($(filter gta3xlwifi, $(TARGET_DEVICE)),)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
